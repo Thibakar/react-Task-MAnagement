ba25599f52dbc8e16a846feb857ac55a0c17183e
Thepakar M <ThepakarM@ge.com>
2021-05-26 08:38:36 +0530

    loginPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
-import TodoList from './components/TodoList'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'font-awesome/css/font-awesome.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+import { Route,  BrowserRouter as Router } from 'react-router-dom';
+
+
+import LoginPage from './components/Login';
+import TodoList from './components/TodoList';
+
 
 function App() {
   return (
     <div className="App">
-      <TodoList />
+       <Router>
+        <div>
+          <Route exact path="/" component={LoginPage} />
+          <Route path="/TodoList" component={TodoList} />
+        </div>
+        </Router>
     </div>
   );
 }
